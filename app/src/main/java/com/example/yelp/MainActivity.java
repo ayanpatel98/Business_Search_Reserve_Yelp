703ad458e0c93c2d252be0c22b61181d640a207d
ayanpatel98 <66831087+ayanpatel98@users.noreply.github.com>
2022-11-18 23:13:43 -0800

    inintial

--- app/src/main/java/com/example/yelp/MainActivity.java
+++ app/src/main/java/com/example/yelp/MainActivity.java
@@ -3,12 +3,55 @@ package com.example.yelp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        RequestQueue mRequestQueue;
+        StringRequest mStringRequest;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner categories =  findViewById(R.id.categoryDropdown);
+        String[] items = new String[]{"Default", "Arts and Entertainment", "Health and Medical", "Hotels and Travel", "Food", "Professional Services"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        categories.setAdapter(adapter);
+
+
+        String url = "https://api-dot-business-search-reserve-081998.uw.r.appspot.com/autocomplete?text=pizza";
+
+        // RequestQueue initialized
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        // String Request initialized
+        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("1st", response);
+                Toast.makeText(getApplicationContext(), "Response :" + response.toString(), Toast.LENGTH_LONG).show();//display the response on screen
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+        mRequestQueue.add(mStringRequest);
+
     }
 }
