c5fba313ea030b1c6c84041c5b3abfbfea8298ee
ayanpatel98 <66831087+ayanpatel98@users.noreply.github.com>
2022-11-23 19:37:05 -0800

    updates

--- app/src/main/java/com/example/yelp/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/yelp/adapter/RecyclerViewAdapter.java
@@ -1,6 +1,9 @@
 package com.example.yelp.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +15,12 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.yelp.R;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
@@ -37,6 +42,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
         JSONObject business = businessList.get(position);
+        holder.srNo.setText(String.valueOf(position+1));
         try {
             holder.busiName.setText(business.getString("name"));
         } catch (JSONException e) {
@@ -48,10 +54,19 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             e.printStackTrace();
         }
         try {
-            holder.distanceTo.setText(String.valueOf((business.getString("distance"))));
+            holder.distanceTo.setText(String.valueOf((Math.round(Float.parseFloat(business.getString("distance").trim()))/1609)));
         } catch (JSONException e) {
             e.printStackTrace();
         }
+//        Image Load ONE WAY
+        try {
+            Picasso.get().load(business.getString("image_url")).into(holder.busiImage); // Add https otherwise t won't work
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+//        IMAGE LOAD, OTHER WAY, NOT WORKING
+//        Bitmap myBitmap = BitmapFactory.decodeFile("https://i.imgur.com/DvpvklR.png");
+//        holder.busiImage.setImageBitmap(myBitmap);
     }
 
     @Override
@@ -60,6 +75,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        public TextView srNo;
         public ImageView busiImage;
         public TextView busiName;
         public TextView rating;
@@ -67,9 +83,11 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            srNo = itemView.findViewById(R.id.srNo);
             busiName = itemView.findViewById(R.id.busiName);
             rating = itemView.findViewById(R.id.rating);
             distanceTo = itemView.findViewById(R.id.distanceTo);
+            busiImage = itemView.findViewById(R.id.busiImage);
         }
 
         @Override
