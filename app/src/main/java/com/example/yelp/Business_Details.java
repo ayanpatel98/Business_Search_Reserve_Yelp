86ade05c4ae98694592c2b621f1b4f93a43f13f3
ayanpatel98 <66831087+ayanpatel98@users.noreply.github.com>
2022-11-24 19:59:41 -0800

    updates

---
+++ app/src/main/java/com/example/yelp/Business_Details.java
@@ -0,0 +1,16 @@
+package com.example.yelp.adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.yelp.R;
+
+public class Business_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_details);
+    }
+}
---
+++ app/src/main/java/com/example/yelp/adapter/VPAdapter.java
@@ -0,0 +1,2 @@
+package com.example.yelp.adapter;public class VPAdapter {
+}
